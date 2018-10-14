51945fdb124e78cedb2c67b2f32a20f6c86cc29d
Raitis Rasmanis <rasmanis.raitis@gmail.com>
2018-10-14 22:15:35 +0300

    Moved all logic in one file, added automatic yesterday date

--- 02_extractor.py
+++ 02_extractor.py
@@ -2,12 +2,10 @@ import datetime
 import os
 import tarfile
 
-now = datetime.datetime.now()
+now = datetime.datetime.today() - datetime.timedelta(1)
 year, month, day = now.strftime("%Y,%m,%d").split(',')
 
-day = '02'
-
-filename = '{}_{}_{}.tar.gz'.format(day,month,year)
+filename = '{}_{}_{}.tar.gz'.format(day, month, year)
 
 tar = tarfile.open(filename, "r:gz")
 tar.extractall('xmls')
